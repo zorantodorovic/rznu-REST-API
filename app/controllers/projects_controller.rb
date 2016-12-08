742d5968e3b20461a5d006c8b081993e4c7df171
ztodorovic <zoran336@gmail.com>
2016-12-08 19:55:46 +0100

    Updates many many updates.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,53 @@
 class ProjectsController < ApplicationController
 
+	before_action :set_user
+
   def index
-    projects = Project.all
+    projects = @user.projects
     render json: projects
   end
 
   def show
-  	project = Project.find(params[:id])
-  	puts project
+  	project = @user.projects.find(params[:id])
   	render json: project
   end
+
+  def create
+  	project = Project.new(project_params)
+    if project.save
+      render json: project, status: :created
+    else
+      render_error(project, :unprocessable_entity)
+    end
+  end
+
+  def update
+  	if @project.update_attributes(project_params)
+      render json: @project, status: :ok
+    else
+      render_error(@project, :unprocessable_entity)
+    end
+  end
+
+  def destroy
+  	@project.destroy
+    head 204
+  end
+
+  private
+
+  def project_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+  end
+
+  def set_user
+    begin
+      @user = User.find params[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      user = User.new
+      user.errors.add(:id, "Wrong ID provided")
+      render_error(user, 404) and return
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :projects
+  # resources :projects
+
+  resources :users do
+	resources :projects  	
+  end
 
   post    'sessions'     => 'sessions#create'
   delete  'sessions/:id' => 'sessions#destroy'
+
 end
