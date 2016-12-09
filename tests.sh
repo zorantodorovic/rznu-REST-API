9239b7350c9c0d17c725acab62cf674ff781e5a0
Zoran Todorovic <zoran@arsfutura.co>
2016-12-09 17:13:36 +0100

    Updated log.

--- tests.sh
+++ tests.sh
@@ -117,34 +117,3 @@ fi
 
 
 
-
-
-
-# echo "Second test : Login should fail"
-
-# JEDAN = curl -X POST -H "Content-Type: application/json" http://localhost:3000/sessions -d '{"username":"invaliduser@fer.hr",
-# "password":"password"}' -w "%{http_code}" > out1 2> /dev/null
-# if [ "$JEDAN" != "406" ] 
-# then
-# 	echo "Second test - FAIL"
-# 	echo "$JEDAN"
-# else
-# 	echo "Second test - OK"
-# fi
-
-# echo "Second test : Invalid login"
-# curl -H "Accept: application/json" http://localhost:3000/posts/1 -u wrong@user:12345678 > out2 2> /dev/null
-# if diff out2 expected2 > /dev/null ; then
-# 	echo "Second test - OK"
-# else
-# 	echo "Second test - FAIL"
-# fi
-
-# echo "First test : Post GET"
-
-# curl -H "Accept: application/json" http://localhost:3000/posts/1 -u luka.dulcic@fer.hr:12345678 > out3 2> /dev/null
-# if diff out1 expected1 > /dev/null ; then
-# 	echo "First test - OK"
-# else
-# 	echo "First test - FAIL"
-# fi
